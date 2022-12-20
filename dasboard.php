6df741453019c50e5bbe277b76f23a96b9e57294
Asma Syafira <syafirasma@gmail.com>
2022-12-20 23:46:30 +0700

    ...

--- dasboard.php
+++ dasboard.php
@@ -103,7 +103,7 @@
     <!-- Exclusive Offers Section -->
     <section class="offers pt-120 reveal fade-bottom">
       <div class="container">
-        <div class="row g-4 justify-content-center align-items-center">
+        <div class="row g-4 justify-content-center align-items-center" >
           <div class="col-sm-12 col-md-8">
             <h1>Treat Yourseld on <br> Exclusive Offers</h1>
           </div>
@@ -111,34 +111,34 @@
             <a href="destination2.php" class="btn btn-secondary rounded-pill">Discover More</a>
           </div>
           <div class="container">
-        <div class="row gx-5">
-          <?php
-          require_once 'koneksi.php';
-          $query = $koneksi->query("SELECT * FROM `room` ORDER BY `price` ASC") or die(mysql_error()); 
-          while($fetch = $query->fetch_array()){
-          ?>
-          <div class="cat-wrap col-xl-4 col-md-6 col-sm-12 my-4">
-            <div class="cat-card card shadow shadow-md rounded-4 p-4" style="border: none !important;">
-              <br>
-              <br>
-              <br>
-              <br>
-              <img src="<?php echo $fetch['photo'] ?>" class="card-img-top" alt="...">
-              <div class="card-body py-4 px-0">
-                <h4 class="card-title"><?php echo $fetch['room_name'] ?></h4>
-                <h6 class="card-title"><?php echo $fetch['room_place'] ?></h6>
-                <hr>
-                <p class="card-text">
-                  <?php echo $fetch['description'] ?>
-                </p>
-                <h5 class="mb-4">Rp. <?php echo $fetch['price'] ?> / Day</h5>
-                <a href="destination3.php?room_id=<?php echo $fetch['room_id']?>" class="btn btn-primary fw-semibold rounded-pill w-100">Book Now</a>
+            <div class="row gx-5">
+            <?php
+            require_once 'koneksi.php';
+            $query = $koneksi->query("SELECT * FROM `room` ORDER BY `price` ASC") or die(mysql_error()); 
+            while($fetch = $query->fetch_array()){
+            ?>
+            <div class="cat-wrap col-xl-4 col-md-6 col-sm-12 my-4">
+              <div class="cat-card card shadow shadow-md rounded-4 p-4" style="border: none !important;">
+                <br>
+                <br>
+                <br>
+                <br>
+                <img src="<?php echo $fetch['photo'] ?>" class="card-img-top" alt="...">
+                <div class="card-body py-4 px-0">
+                  <h4 class="card-title"><?php echo $fetch['room_name'] ?></h4>
+                  <h6 class="card-title"><?php echo $fetch['room_place'] ?></h6>
+                  <hr>
+                  <p class="card-text">
+                    <?php echo $fetch['description'] ?>
+                  </p>
+                  <h5 class="mb-4">Rp. <?php echo $fetch['price'] ?> / Day</h5>
+                  <a href="destination3.php?room_id=<?php echo $fetch['room_id']?>" class="btn btn-primary fw-semibold rounded-pill w-100">Book Now</a>
+                </div>
               </div>
             </div>
+            <?php } ?>
           </div>
-          <?php } ?>
         </div>
-      </div>
 
         </div>
       </div>
--- index.php
+++ index.php
@@ -24,32 +24,32 @@
 <body>
 	<!--========== NAVBAR ==========-->
 	<nav class="navbar navbar-expand-lg bg-primary navbar-dark fixed-top shadow shadow-md">
-    <div class="container">
-      <a class="navbar-brand" href="index.html"><img src="./assets/img/Logo.png" alt="Logo"></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-          <li class="nav-item mx-2">
-            <a class="nav-link active fs-5" href="#home">Home</a>
-          </li>
-          <li class="nav-item mx-2">
-            <a class="nav-link fs-5" href="#tentangkami">Tentang Kami</a>
-          </li>
-          <li class="nav-item mx-2">
-            <a class="nav-link fs-5" href="#">Meet our Team</a>
-          </li>
-        </ul>
-        <a href="login.php" class="text-decoration-none">
-          <div class="d-flex btn btn-primary p-3 rounded-pill justify-content-center align-items-center">
-            <span class="ms-3 text-primary fw-semibold">Masuk/Daftar</span>
-          </div>
-        </a>
-      </div>
-    </div>
-  </nav>
+		<div class="container">
+			<a class="navbar-brand" href="index.html"><img src="./assets/img/Logo.png" alt="Logo"></a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+				<li class="nav-item mx-2">
+					<a class="nav-link active fs-5" href="#home">Home</a>
+				</li>
+				<li class="nav-item mx-2">
+					<a class="nav-link fs-5" href="#tentangkami">About Us</a>
+				</li>
+				<li class="nav-item mx-2">
+					<a class="nav-link fs-5" href="#">Our Team</a>
+				</li>
+				</ul>
+				<a href="login.php" class="text-decoration-none">
+				<div class="d-flex btn btn-primary p-3 rounded-pill justify-content-center align-items-center">
+					<span class="ms-3 text-primary fw-semibold">Log In/Register</span>
+				</div>
+				</a>
+			</div>
+		</div>
+  	</nav>
 	<!--========== END OF NAVBAR ==========-->
 
     <!-- Back to top button -->
@@ -77,117 +77,119 @@
     </section>
     <!-- end jumbotron -->
 
-		<section class="about mt-5 pt-5" id="tentangkami">
-			<div class="container h-100">
-				<div class="row h-100 gx-5">
-					<div class="col-6 d-flex flex-column justify-content-center">
-						<img src="assets/img/vilaaaaa.png"
-							alt="About Villa La Land" class="img-fluid p-5">
-					</div>
-					<div class="col-6 d-flex flex-column justify-content-center">
-						<h2>Tentang Kami</h2>
-						<p>
-							Villa La Land adalah Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto
-							ab, tenetur repellendus corrupti exercitationem expedita quasi fugit excepturi obcaecati
-							nihil in, qui ratione, officia sint illum consectetur iusto. Odit, mollitia!
-						</p>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quod excepturi blanditiis est
-							laboriosam mollitia! Ad nisi quibusdam deleniti, nostrum ipsam, accusantium reiciendis
-							dolorum dolore error, architecto sit nihil maiores? Vel quae error natus accusantium!
-						</p>
-						<span><a href="#about" class="btn btn-primary px-3 mt-4 fw-bold">About Villa La Land</a></span>
-					</div>
+	<section class="about mt-5 pt-5" id="tentangkami">
+		<div class="container h-100">
+			<div class="row h-100 gx-5">
+				<div class="col-6 d-flex flex-column justify-content-center">
+					<img src="assets/img/vilaaaaa.png"
+						alt="About Villa La Land" class="img-fluid p-5">
+				</div>
+				<div class="col-6 d-flex flex-column justify-content-center">
+					<h2>About Us</h2>
+					<p>
+					Our website always works professionally by providing the best for users in order to give users a sense of security, comfort and satisfaction with the services of our website.
+
+We want to help recommend the best tourist attractions for your family, friends, or your partner. By sharing insights from experienced users, you can find interesting and interesting tourist attractions.
+					</p>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quod excepturi blanditiis est
+						laboriosam mollitia! Ad nisi quibusdam deleniti, nostrum ipsam, accusantium reiciendis
+						dolorum dolore error, architecto sit nihil maiores? Vel quae error natus accusantium!
+					</p>
+					<span><a href="#about" class="btn btn-primary px-3 mt-4 fw-bold">About Villa La Land</a></span>
 				</div>
 			</div>
-		</section>
-		<section class="film">
-			<div class="container p-5">
-				<div class="row p-5">
-					<div class="col p-5 bg-warning shadow shadow-lg">
-						<iframe width="100%" height="500" src="https://www.youtube.com/embed/Fez9jsONpRE"
-							title="YouTube video player" frameborder="0"
-							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-							allowfullscreen></iframe>
-					</div>
+		</div>
+	</section>
+
+	<section class="film">
+		<div class="container p-5">
+			<div class="row p-5">
+				<div class="col p-5 bg-warning shadow shadow-lg">
+					<iframe width="100%" height="500" src="https://www.youtube.com/embed/Fez9jsONpRE"
+						title="YouTube video player" frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+						allowfullscreen></iframe>
 				</div>
 			</div>
-		</section>
+		</div>
+	</section>
 		
 		<!-- maps -->
 	<div class="container mt-5">
-            <h1 class="judul text-center mb-5">Temukan kami</h3>
-            <div id="map-container-google-2" class="z-depth-1-half map-container" style="height: 500px">
-              <iframe
-                src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Jl. A. Yani No.212c, Mendungan, Pabelan, Kec. Kartasura, Kabupaten Sukoharjo, Jawa Tengah 57161&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-                width="100%"
-                height="450"
-                style="border: 0"
-                allowfullscreen=""
-                loading="lazy"
-              ></iframe>
-            </div>
-          </div>
-       </div>
-       <!-- akhir maps-->
+		<h1 class="judul text-center mb-5">Temukan kami</h3>
+		<div id="map-container-google-2" class="z-depth-1-half map-container" style="height: 500px">
+			<iframe
+			src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Jl. A. Yani No.212c, Mendungan, Pabelan, Kec. Kartasura, Kabupaten Sukoharjo, Jawa Tengah 57161&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+			width="100%"
+			height="450"
+			style="border: 0"
+			allowfullscreen=""
+			loading="lazy"
+			></iframe>
+		</div>
+		</div>
+	</div>
+    <!-- akhir maps-->
 
-	   <section class="about mt-5 pt-5" id="about">
-			<div class="container h-100">
-				<div class="row h-100 gx-5">
-	   <section class="about">
-			<div class="container h-100">
-				<div class="row h-100 d-flex align-items-center">
-					<div class="col-xl-4 mb-3">
-						<div class="card">
-							<div class="card-header py-3">
-								<h5 class="text-center"><strong>Keunggulan 1</strong></h5>
-							</div>
-							<div class="card-body">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vel neque laboriosam
-								assumenda inventore sint dolorum exercitationem repellendus, laborum sunt quidem
-								distinctio, dolore maiores laudantium. Dicta facere culpa ex tenetur!
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vel neque laboriosam
-								assumenda inventore sint dolorum exercitationem repellendus, laborum sunt quidem
-								distinctio, dolore maiores laudantium. Dicta facere culpa ex tenetur!
+	<section class="about mt-5 pt-5" id="about">
+		<div class="container h-100">
+			<div class="row h-100 gx-5">
+				<section class="about">
+						<div class="container h-100">
+							<div class="row h-100 d-flex align-items-center">
+								<div class="col-xl-4 mb-3">
+									<div class="card">
+										<div class="card-header py-3">
+											<h5 class="text-center"><strong>Keunggulan 1</strong></h5>
+										</div>
+										<div class="card-body">
+											Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vel neque laboriosam
+											assumenda inventore sint dolorum exercitationem repellendus, laborum sunt quidem
+											distinctio, dolore maiores laudantium. Dicta facere culpa ex tenetur!
+											Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vel neque laboriosam
+											assumenda inventore sint dolorum exercitationem repellendus, laborum sunt quidem
+											distinctio, dolore maiores laudantium. Dicta facere culpa ex tenetur!
+										</div>
+									</div>
+								</div>
+								<div class="col-xl-4">
+									<div class="card">
+										<div class="card-header py-3">
+											<h5 class="text-center"><strong>Keunggulan 2</strong></h5>
+										</div>
+										<div class="card-body">
+											Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vel neque laboriosam
+											assumenda inventore sint dolorum exercitationem repellendus, laborum sunt quidem
+											distinctio, dolore maiores laudantium. Dicta facere culpa ex tenetur!
+											Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vel neque laboriosam
+											assumenda inventore sint dolorum exercitationem repellendus, laborum sunt quidem
+											distinctio, dolore maiores laudantium. Dicta facere culpa ex tenetur!
+										</div>
+									</div>
+								</div>
+								<div class="col-xl-4">
+									<div class="card">
+										<div class="card-header py-3">
+											<h5 class="text-center"><strong>Keunggulan 3</strong></h5>
+										</div>
+										<div class="card-body">
+											Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vel neque laboriosam
+											assumenda inventore sint dolorum exercitationem repellendus, laborum sunt quidem
+											distinctio, dolore maiores laudantium. Dicta facere culpa ex tenetur!
+											Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vel neque laboriosam
+											assumenda inventore sint dolorum exercitationem repellendus, laborum sunt quidem
+											distinctio, dolore maiores laudantium. Dicta facere culpa ex tenetur!
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="col-xl-4">
-						<div class="card">
-							<div class="card-header py-3">
-								<h5 class="text-center"><strong>Keunggulan 2</strong></h5>
-							</div>
-							<div class="card-body">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vel neque laboriosam
-								assumenda inventore sint dolorum exercitationem repellendus, laborum sunt quidem
-								distinctio, dolore maiores laudantium. Dicta facere culpa ex tenetur!
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vel neque laboriosam
-								assumenda inventore sint dolorum exercitationem repellendus, laborum sunt quidem
-								distinctio, dolore maiores laudantium. Dicta facere culpa ex tenetur!
-							</div>
-						</div>
-					</div>
-					<div class="col-xl-4">
-						<div class="card">
-							<div class="card-header py-3">
-								<h5 class="text-center"><strong>Keunggulan 3</strong></h5>
-							</div>
-							<div class="card-body">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vel neque laboriosam
-								assumenda inventore sint dolorum exercitationem repellendus, laborum sunt quidem
-								distinctio, dolore maiores laudantium. Dicta facere culpa ex tenetur!
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vel neque laboriosam
-								assumenda inventore sint dolorum exercitationem repellendus, laborum sunt quidem
-								distinctio, dolore maiores laudantium. Dicta facere culpa ex tenetur!
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	   
-	
-	<!--kureng footer plissssssssssssssss-->
-	
+					</section>
+				</section>
+		<?php include "footer.php" ?>
+
+	</main>
+
 	<!-- Option 2: Separate Popper and Bootstrap JS -->
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
 		integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">

ee292944ac1125c3dc55aedc995e90b37db72edd
Aldy <maldyfaturrohman@gmail.com>
2022-12-21 18:51:32 +0700

    fix transaksi

--- transaksi.php
+++ transaksi.php
@@ -58,7 +58,7 @@
   <?php
   if (isset($_POST['yes'])) { 
      $koneksi->query("UPDATE `transaction` SET hargaTotal = '$diBayar' WHERE transaction_id = '$transactionInput'") or die(mysqli_error());
-     header("location: dasboard.php");
+     header("location: dashboard.php");
    }
 
   ?>

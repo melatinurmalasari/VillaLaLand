82cf42bdf7184453125fb9a63a87f5956f34fe92
melatinurmalasari <melati.nur.malasari8@student.uns.ac.id>
2022-12-21 21:06:30 +0700

    admin crud add

--- admin/tambah_aksi.php
+++ admin/tambah_aksi.php
@@ -1,7 +1,11 @@
 <?php 
 // koneksi database
 include 'koneksi.php';
+
  
+// menangkap data yang di kirim dari form
+
+
 $photo = $_POST['photo'];
 $room_name = $_POST['room_name'];
 $room_place = $_POST['room_place'];
@@ -11,10 +15,9 @@ $bathroom = $_POST['bathroom'];
 $wifi = $_POST['wifi'];
 $pool = $_POST['pool'];
 $description = $_POST['description'];
-
  
 // menginput data ke database
-mysqli_query($koneksi,"insert into room values('','$photo','$room_name','$room_place', '$price', '$bedroom', '$bathroom', '$wifi', '$pool', '$description')");
+mysqli_query($koneksi,"insert into room(`room_name`, `room_place`, `description`, `price`, `photo`, `bedroom`, `bathroom`, `wifi`, `pool`) values('$room_name','$room_place','$description', '$price', '$photo', '$bedroom', '$bathroom', '$wifi', '$pool')");
  
 // mengalihkan halaman kembali ke ke awal
 header("location:villabooking.php");

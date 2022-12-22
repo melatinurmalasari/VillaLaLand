b1e0e2a244bde368abe177c5706da706ae828acf
melatinurmalasari <melati.nur.malasari8@student.uns.ac.id>
2022-12-22 19:56:25 +0700

    update bosssss

--- admin/datacus.php
+++ admin/datacus.php
@@ -1,75 +1,143 @@
+<?php
+require_once('koneksi.php');
+?>
 
-<html>
+<!doctype html>
+<html lang="en">
+
 <head>
-<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
-    <title>Bootstrap Example</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <title>Villa Booking </title>
-    <style>
-        table,tr,td {
-            border: 1px solid black;
-        }
-        thead {
-            background-color: #cccddd;
-        }
-    </style>
-</head>
-<body class="p-3 m-0 border-0 bd-example">
-    <?php include "header.php" ?>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <main>
-    <section class="destinations pt-120">
-      <div class="container">
-        <div class="row gx-5">
-          <div class="col-12 d-flex align-items-center justify-content-center mb-3 text-center">
-            <h1 class="fw-bold">Data transaksi</h1>
-            
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!-- Data Tables -->
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
+
+  <script type="text/javascript">
+    const showInfo = (value) => {
+      const xhttp = new XMLHttpRequest();
+      xhttp.onload = function() {
+        alert(this.responseText)
+      }
+      var id = value
+      xhttp.open("POST", "action.php?aksi=info");
+      xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xhttp.send("id=" + id)
+    }
+
+    const deleteData = (value) => {
+      const xhttp = new XMLHttpRequest();
+      xhttp.onload = function() {
+        alert(this.responseText)
+      }
+      var id = value
+      xhttp.open("POST", "action.php?aksi=delete");
+      xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      let con = confirm("Apakah anda yakin ingin menghapus data?")
+      if (con) {
+        xhttp.send("id=" + id)
+      }
+    }
+  </script>
+
+  <title></title>
+</head>
+
+<body>
+  <!-- Navbar -->
+  <header>
+    <nav class=" navbar navbar-expand-lg navbar-light bg-light">
+      <div class=" container-fluid">
+        <div class=" collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class=" nav-item"><a class=" nav-link" href="villabooking.php">Villa</a></li>
+            <li class=" nav-item"><a class=" nav-link" href="datacus.php">Transaksion</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <!-- Main UI -->
+  <main>
+    
+
+          <div class="card">
+            <div class="card-body">
+              <div class="card-title">Transaction</div>
+              <hr>
+              <?php
+              $result = mysqli_query($koneksi, "SELECT * FROM transaction");
+              ?>
+
+              <table style="width: 100%;" class="display" id="tabel-data">
+                <thead>
+                  <tr>
+                  
+                    
+                    <th>Id Transaction</th>
+                    <th>Id Guest</th>  
+                    <th>Id Room</th>
+                    <th>Checkin</th>
+                    <th>Checkout</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>No Contact</th>  
+                    <th>Email</th>  
+                    <th>#</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  while ($data = mysqli_fetch_array($result)) {
+                  ?>
+
+                    <tr>
+                    <td><?php echo $data['transaction_id'] ?></td>
+                    <td><?php echo $data['guest_id'] ?></td>
+                    <td><?php echo $data['room_id'] ?></td>
+                    <td><?php echo $data['checkin'] ?></td>
+                    <td><?php echo $data['checkout'] ?></td>
+                    <td><?php echo $data['first_name'] ?></td>
+                    <td><?php echo $data['last_name'] ?></td>
+                    <td><?php echo $data['contactno'] ?></td>
+                    <td><?php echo $data['email'] ?></td>
+                    <td>
+                        <a href="" class="btn btn-info">Konfirmasi</a>
+                    </td>
+                    </tr>
+
+                  <?php } ?>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-    </section>
-    <table class="table caption-top">
-        <thead>
-            <tr>
-                <th scope="col">No</th>
-                <th scope="col">Id Transaction</th>
-                <th scope="col">Id Guest</th>  
-                <th scope="col">Id Room</th>
-                <th scope="col">Checkin</th>
-                <th scope="col">Checkout</th>
-                <th scope="col">First Name</th>
-                <th scope="col">Last Name</th>
-                <th scope="col">No Contact</th>  
-                <th scope="col">Email</th>  
-                <th scope="col">#</th>                     
-            </tr>
-        </thead>
-        <?php
-        include "koneksi.php";
-        $no = 1;
-        $query = mysqli_query($koneksi, 'SELECT * FROM transaction');
-        while ($data = mysqli_fetch_array($query)) {
-        ?>
-            <tr>
-                <td><?php echo $no++ ?></td>
-                <td><?php echo $data['transaction_id'] ?></td>
-                <td><?php echo $data['guest_id'] ?></td>
-                <td><?php echo $data['room_id'] ?></td>
-                <td><?php echo $data['checkin'] ?></td>
-                <td><?php echo $data['checkout'] ?></td>
-                <td><?php echo $data['first_name'] ?></td>
-                <td><?php echo $data['last_name'] ?></td>
-                <td><?php echo $data['contactno'] ?></td>
-                <td><?php echo $data['email'] ?></td>
-                <td>
-                    <a href="" class="btn btn-info">Konfirmasi</a>
-                </td>
-                
-            </tr>
-        <?php } ?>
-    </table>
+    </div>
+  </main>
+
+
+  <!-- Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <!-- Data Tables -->
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $('#tabel-data').DataTable({
+        order: [
+          [2, 'desc']
+        ],
+        info: false,
+        searching: false
+      });
+    });
+  </script>
 </body>
-</html>
+
+
+
+
--- admin/villabooking.php
+++ admin/villabooking.php
@@ -1,81 +1,144 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
-    <title>Bootstrap Example</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <title>Villa Booking </title>
-    <style>
-        table,tr,td {
-            border: 1px solid black;
-        }
-        thead {
-            background-color: #cccddd;
-        }
-    </style>
-</head>
-<body class="p-3 m-0 border-0 bd-example">
-    <?php include "header.php" ?>
+<?php
+require_once('koneksi.php');
+?>
 
-    <main>
-    <section class="destinations pt-120">
-      <div class="container">
-        <div class="row gx-5">
-          <div class="col-12 d-flex align-items-center justify-content-center mb-3 text-center">
-            <h1 class="fw-bold">Data Villa</h1>
-            
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!-- Data Tables -->
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
+
+  <script type="text/javascript">
+    const showInfo = (value) => {
+      const xhttp = new XMLHttpRequest();
+      xhttp.onload = function() {
+        alert(this.responseText)
+      }
+      var id = value
+      xhttp.open("POST", "action.php?aksi=info");
+      xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xhttp.send("id=" + id)
+    }
+
+    const deleteData = (value) => {
+      const xhttp = new XMLHttpRequest();
+      xhttp.onload = function() {
+        alert(this.responseText)
+      }
+      var id = value
+      xhttp.open("POST", "action.php?aksi=delete");
+      xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      let con = confirm("Apakah anda yakin ingin menghapus data?")
+      if (con) {
+        xhttp.send("id=" + id)
+      }
+    }
+  </script>
+
+  <title></title>
+</head>
+
+<body>
+  <!-- Navbar -->
+  <header>
+    <nav class=" navbar navbar-expand-lg navbar-light bg-light">
+      <div class=" container-fluid">
+        <div class=" collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class=" nav-item"><a class=" nav-link" href="villabooking.php">Villa</a></li>
+            <li class=" nav-item"><a class=" nav-link" href="datacus.php">Transaksion</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <!-- Main UI -->
+  <main>
+    
+
+          <div class="card">
+            <div class="card-body">
+              <div class="card-title">Villa</div>
+              <hr>
+              <?php
+              $result = mysqli_query($koneksi, "SELECT * FROM room");
+              ?>
+
+              <table style="width: 100%;" class="display" id="tabel-data">
+                <thead>
+                  <tr>
+                  <a href="addvilla.php" class="btn btn-light">+Add Villa</a></caption>
+                    
+                    <th>Id Villa</th>
+                    <th>Photo</th>  
+                    <th>Villa name</th>
+                    <th>Place</th>
+                    <th>Price</th>
+                    <th>Bedroom</th>
+                    <th>Bathroom</th>
+                    <th>wifi</th>  
+                    <th>pool</th>  
+                    <th>Description</th>
+                    <th>#</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  while ($data = mysqli_fetch_array($result)) {
+                  ?>
+
+                    <tr>
+                    
+                    <td><?php echo $data['room_id'] ?></td>
+                    <td><?php echo $data['photo'] ?></td>
+                    <td><?php echo $data['room_name'] ?></td>
+                    <td><?php echo $data['room_place'] ?></td>
+                    <td><?php echo $data['price'] ?></td>
+                    <td><?php echo $data['bedroom'] ?></td>
+                    <td><?php echo $data['bathroom'] ?></td>
+                    <td><?php echo $data['wifi'] ?></td>
+                    <td><?php echo $data['pool'] ?></td>
+                    <td><?php echo $data['description'] ?></td>
+                    <td>
+                        <a href="tampilById.php?room_id=<?php echo $data['room_id'] ?>" class="btn btn-info">Detail</a>
+                        <a href="edit.php?room_id=<?= $data['room_id'] ?>" class="btn btn-success">Edit</a>
+                        <a href="delete.php?room_id=<?php echo $data['room_id']?>" class="btn btn-danger">Delete</a>
+                    </td>
+                    </tr>
+
+                  <?php } ?>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-    </section>
-    <table class="table caption-top">
-        <thead>
-            <tr>
-                <caption>
-                <a href="addvilla.php" class="btn btn-light">+Add Villa</a></caption>
-                <th scope="col">No</th>
-                <th scope="col">Id Villa</th>
-                <th scope="col">Photo</th>  
-                <th scope="col">Villa name</th>
-                <th scope="col">Place</th>
-                <th scope="col">Price</th>
-                <th scope="col">Bedroom</th>
-                <th scope="col">Bathroom</th>
-                <th scope="col">wifi</th>  
-                <th scope="col">pool</th>  
-                <th scope="col">Description</th>
-                <th scope="col">#</th>                     
-            </tr>
-        </thead>
-        <?php
-        include "koneksi.php";
-        $no = 1;
-        $query = mysqli_query($koneksi, 'SELECT * FROM room');
-        while ($data = mysqli_fetch_array($query)) {
-        ?>
-            <tr>
-                <td><?php echo $no++ ?></td>
-                <td><?php echo $data['room_id'] ?></td>
-                <td><?php echo $data['photo'] ?></td>
-                <td><?php echo $data['room_name'] ?></td>
-                <td><?php echo $data['room_place'] ?></td>
-                <td><?php echo $data['price'] ?></td>
-                <td><?php echo $data['bedroom'] ?></td>
-                <td><?php echo $data['bathroom'] ?></td>
-                <td><?php echo $data['wifi'] ?></td>
-                <td><?php echo $data['pool'] ?></td>
-                <td><?php echo $data['description'] ?></td>
-                <td>
-                    <a href="tampilById.php?room_id=<?php echo $data['room_id'] ?>" class="btn btn-info">Detail</a>
-                    <a href="edit.php?room_id=<?= $data['room_id'] ?>" class="btn btn-success">Edit</a>
-                    <a href="delete.php?room_id=<?php echo $data['room_id']?>" class="btn btn-danger">Delete</a>
-                </td>
-                
-            </tr>
-        <?php } ?>
-    </table>
+    </div>
+  </main>
+
+
+  <!-- Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <!-- Data Tables -->
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $('#tabel-data').DataTable({
+        order: [
+          [2, 'desc']
+        ],
+        info: false,
+        searching: false
+      });
+    });
+  </script>
 </body>
-</html>
